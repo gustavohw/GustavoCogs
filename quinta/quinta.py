1c67ba28c5c13fdbb078c4ee7dff83196c9cdfe5
Gustavo Walendowsky <gustavohw8@gmail.com>
2017-08-21 19:20:01 -0300

    text edit

--- quinta/quinta.py
+++ quinta/quinta.py
@@ -6,7 +6,7 @@ class Quinta:
 
     def __init__(self, bot):
         self.bot = bot
-        self.fortune = "CHEGAMOS NA QUINTA FEIRA, ÚLTIMO GÁS" \
+        self.fortune = "CHEGAMOS NA QUINTA FEIRA, ÚLTIMO GÁS " \
                        "https://www.youtube.com/watch?v=OZpx3loLxg8"
 
     @commands.command(name="quinta")
