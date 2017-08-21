33e61be4ae997b4035e654157e7cb77585812782
Gustavo Walendowsky <gustavohw8@gmail.com>
2017-08-21 19:35:56 -0300

    played cog initial

--- played/played.py
+++ played/played.py
@@ -2,18 +2,18 @@ from discord.ext import commands
 import time
 
 
-class Quinta:
+class Played:
 
     def __init__(self, bot):
         self.bot = bot
         self.fortune = "CHEGAMOS NA QUINTA FEIRA, ÚLTIMO GÁS " \
                        "https://www.youtube.com/watch?v=OZpx3loLxg8"
 
-    @commands.command(name="quinta")
-    async def _cookie(self):
+    @commands.command(name="played")
+    def _cookie(self):
         time.sleep(5)
         return await self.bot.say(self.fortune)
 
 
 def setup(bot):
-    bot.add_cog(Quinta(bot))
+    bot.add_cog(Played(bot))
