360cfc0c9411333ceb065ddeff8b0bc8059fac8b
Gustavo Walendowsky <gustavohw8@gmail.com>
2017-08-21 17:37:44 -0300

    added quinta test cog

---
+++ quinta.py
@@ -0,0 +1,15 @@
+import os
+from discord.ext import commands
+
+class Quinta:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command (no_pm=True, name='quinta')
+    async def _quinta(self, *, text):
+        await self.bot.say("https://www.youtube.com/watch?v=OZpx3loLxg8")
+
+
+    def setup(bot):
+        n = Quinta(bot)
+        bot.add_cog(n)
