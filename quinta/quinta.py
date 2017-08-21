e4c01207dcba69670b0dd33fc8ed819ae76a9d25
Gustavo Walendowsky <gustavohw8@gmail.com>
2017-08-21 19:18:44 -0300

    text edit

--- quinta/quinta.py
+++ quinta/quinta.py
@@ -1,13 +1,13 @@
 # Developed by Redjumpman for Redbot
 from discord.ext import commands
-from random import choice as randchoice
 
 
 class Quinta:
 
     def __init__(self, bot):
         self.bot = bot
-        self.fortune = "https://www.youtube.com/watch?v=OZpx3loLxg8"
+        self.fortune = "CHEGAMOS NA QUINTA FEIRA, ÚLTIMO GÁS" \
+                       "https://www.youtube.com/watch?v=OZpx3loLxg8"
 
     @commands.command(name="quinta")
     async def _cookie(self):
