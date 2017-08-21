66d5fe8ebf62059ee8c5ab87a163bffb66049800
Gustavo Walendowsky <gustavohw8@gmail.com>
2017-08-21 20:11:39 -0300

    added background test code

--- played/played.py
+++ played/played.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from .utils.dataIO import fileIO
 import time
 
+client = discord.Client()
 
 class Played:
 
@@ -11,10 +12,26 @@ class Played:
         self.bot = bot
         self.data_file = 'data/played/played.json'
 
-    @commands.command(name="played")
-    async def _cookie(self):
-        time.sleep(1)
-        return await self.bot.say("teste")
+
+
+    asyncdef my_background_task():
+        await client.wait_until_ready()
+        counter = 0
+        channel = discord.Object(id='channel_id_here')
+        while not client.is_closed:
+            counter += 1
+            await client.send_message(channel, counter)
+            await asyncio.sleep(60)  # task runs every 60 seconds
+
+    @client.event
+    async def on_ready():
+        print('Logged in as')
+        print(client.user.name)
+        print(client.user.id)
+        print('------')
+
+    client.loop.create_task(my_background_task())
+    client.run('token')
 
 
 def check_folder():
