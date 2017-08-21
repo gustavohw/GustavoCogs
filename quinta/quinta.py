038d18ae31661c35586221a5818011904780ed03
Gustavo Walendowsky <gustavohw8@gmail.com>
2017-08-21 17:57:46 -0300

    fix quinta cog

--- quinta/quinta.py
+++ quinta/quinta.py
@@ -5,7 +5,7 @@ class Quinta:
         self.bot = bot
 
     @commands.command (no_pm=True, name='quinta')
-    async def _quinta(self, *, text):
+    async def quinta(self, *, text):
         await self.bot.say("https://www.youtube.com/watch?v=OZpx3loLxg8")
 
 
@@ -13,4 +13,3 @@ def setup(bot):
     n = Quinta(bot)
     bot.add_cog(n)
 
-    
