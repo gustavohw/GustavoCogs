b4400fdade82a8a2d4d85184fdc64dd21a70bfea
Gustavo Walendowsky <gustavohw8@gmail.com>
2017-08-30 17:10:45 -0300

    tidy quinta and remove delay

--- quinta/quinta.py
+++ quinta/quinta.py
@@ -1,19 +1,15 @@
 from discord.ext import commands
-import time
-
 
 class Quinta:
 
     def __init__(self, bot):
         self.bot = bot
-        self.fortune = "CHEGAMOS NA QUINTA FEIRA, ÚLTIMO GÁS " \
+        self.message = "CHEGAMOS NA QUINTA FEIRA, ÚLTIMO GÁS " \
                        "https://www.youtube.com/watch?v=OZpx3loLxg8"
 
     @commands.command(name="quinta")
     async def _cookie(self):
-        time.sleep(5)
-        return await self.bot.say(self.fortune)
-
+        return await self.bot.say(self.message)
 
 def setup(bot):
     bot.add_cog(Quinta(bot))
