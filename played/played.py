9104c7a553e9392db004297b44bd81985b11f43b
Gustavo Walendowsky <gustavohw8@gmail.com>
2018-01-03 15:29:03 -0200

    beautify fix

--- played/played.py
+++ played/played.py
@@ -103,7 +103,7 @@ class Played:
                     msg += '\n'
                     finalMsg += msg
                     i += 1
-            finalMsg += '\nForam jogados totais de <{} horas e {} minutos> nessa semana!'.format(str(total_played_hours), str(total_played_minutes))
+            finalMsg += '\nForam jogados totais de <{} horas> e <{} minutos> nessa semana!'.format(str(total_played_hours), str(total_played_minutes))
             finalMsg += ' ```'
             self.save_last(server)
             await self.bot.say(finalMsg)
