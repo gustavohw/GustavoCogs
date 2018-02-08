3ed80b9eeea4a37d7352412fa585ebf0f5094713
Gustavo Walendowsky <gustavohw8@gmail.com>
2018-02-08 16:40:40 -0200

    save fix?

--- played/played.py
+++ played/played.py
@@ -138,18 +138,20 @@ class Played:
         await send_cmd_help(ctx)
         return
 
-    def save_last(self, server_id):
+    def save_last(self):
         data = fileIO(self.data_file, 'load')
         saved_epoch = data['INFO']['EPOCH']
         weekly_total = 0
         if check_weekly(saved_epoch):
-            for game in data[server_id]['GAMES']:
-                weekly_total += (data[server_id]['GAMES'][game]['MINUTES'] - data[server_id]['GAMES'][game]['LASTPLAY'])
-                data[server_id]['GAMES'][game]['LASTPLAY'] = data[server_id]['GAMES'][game]['MINUTES']
+            for srv in data:
+                if srv != 'INFO':
+                    for game in data[srv]['GAMES']:
+                        weekly_total += (data[srv]['GAMES'][game]['MINUTES'] - data[srv]['GAMES'][game]['LASTPLAY'])
+                        data[srv]['GAMES'][game]['LASTPLAY'] = data[srv]['GAMES'][game]['MINUTES']
 
-            data[server_id]['HISTORY'][str(saved_epoch)] = {}
-            data[server_id]['HISTORY'][str(saved_epoch)]['EPOCH'] = saved_epoch
-            data[server_id]['HISTORY'][str(saved_epoch)]['TIME'] = weekly_total
+                    data[srv]['HISTORY'][str(saved_epoch)] = {}
+                    data[srv]['HISTORY'][str(saved_epoch)]['EPOCH'] = saved_epoch
+                    data[srv]['HISTORY'][str(saved_epoch)]['TIME'] = weekly_total
 
         fileIO(self.data_file, 'save', data)
         save_weekly_epoch()
@@ -171,7 +173,8 @@ class Played:
     @_played.command(pass_context=True, no_pm=True, name='save')
     # @checks.admin_or_permissions(administrator=True)
     async def _played_save(self, context):
-        self.save_last(str(context.message.content)[13:])
+        # self.save_last(str(context.message.content)[13:])
+        self.save_last()
 
     @_played.command(pass_context=True, no_pm=True, name='backup')
     async def _played_backup(self):
