0824dcfc637e685893d4836f16a8312cb1c4d88f
Gustavo Walendowsky <gustavohw8@gmail.com>
2018-02-15 17:30:05 -0200

    debug

--- played/played.py
+++ played/played.py
@@ -194,9 +194,8 @@ class Played:
     @_played.command(pass_context=True, no_pm=True, name='time')
     async def _played_time(self, context):
         data = fileIO(self.data_file, 'load')
-        mes = str(context.message.content)[1:]
-        msg = 'Input time is {}[{}] \n'.format(epoch_converter(mes), mes)
-        msg += 'Current time is {}[{}] \n\n'.format(epoch_converter(data['INFO']['EPOCH']), data['INFO']['EPOCH'])
+        msg = 'Current time is {}[{}] \n'.format(epoch_converter(time.time()), time.time())
+        msg += 'Current saved time is {}[{}] \n\n'.format(epoch_converter(data['INFO']['EPOCH']), data['INFO']['EPOCH'])
         msg += 'Has a week ben passed since saved epoch: {}'.format(check_weekly(data['INFO']['EPOCH']))
 
         await self.bot.say(msg)
